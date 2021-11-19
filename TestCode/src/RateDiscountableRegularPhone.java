793337ba7cc4cae3ed6694ebfd54504cff2b41e9
ghjh0079 <ghjh0079@gmail.com>
2021-11-20 02:55:07 +0900

    Step03

---
+++ TestCode/src/RateDiscountableRegularPhone.java
@@ -0,0 +1,15 @@
+import java.time.Duration;
+
+public class RateDiscountableRegularPhone extends RegularPhone {
+    private Money discountAmount;
+
+    public RateDiscountableRegularPhone(Money amount, Duration seconds, Money discountAmount) {
+        super(amount, seconds);
+        this.discountAmount = discountAmount;
+    }
+
+    @Override
+    protected Money afterCalculated(Money fee) {
+        return fee.minus(discountAmount);
+    }
+}
