00c1ff2d603d6db52edbf0900fdae5263abbdb83
ghjh0079 <ghjh0079@gmail.com>
2021-11-20 02:46:25 +0900

    Step01

---
+++ TestCode/src/Phone.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Phone {
+    private List<Call> calls = new ArrayList<>();
+
+    public Money calculateFee() {
+        Money result = Money.ZERO;
+
+        for(Call call : calls) {
+            result = result.plus(calculateCallFee(call));
+        }
+
+        return result;
+    }
+
+    abstract protected Money calculateCallFee(Call call);
+}
